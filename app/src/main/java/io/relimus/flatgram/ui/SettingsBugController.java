28d68a2a531d732c0987de517e055fce99960c2a
relimus <relimus@proton.me>
2026-07-09 12:11:47 +0800

    ui: ShowPeerIDs switch can be visible without TesterLevel now

--- app/src/main/java/io/relimus/flatgram/ui/SettingsBugController.java
+++ app/src/main/java/io/relimus/flatgram/ui/SettingsBugController.java
@@ -738,14 +738,12 @@ public class SettingsBugController extends RecyclerViewController<SettingsBugCon
         break;
       }
       case Section.EXPERIMENTS: {
-        if (testerLevel >= Tdlib.TesterLevel.TESTER || Settings.instance().isExperimentEnabled(Settings.EXPERIMENT_FLAG_SHOW_PEER_IDS)) {
-          if (!items.isEmpty()) {
-            items.add(new ListItem(ListItem.TYPE_SHADOW_TOP));
-          }
-          items.add(new ListItem(ListItem.TYPE_RADIO_SETTING, R.id.btn_experiment, 0, R.string.Experiment_PeerIds).setLongValue(Settings.EXPERIMENT_FLAG_SHOW_PEER_IDS));
-          items.add(new ListItem(ListItem.TYPE_SHADOW_BOTTOM));
-          items.add(new ListItem(ListItem.TYPE_DESCRIPTION, 0, 0, R.string.Experiment_PeerIdsInfo));
+        if (!items.isEmpty()) {
+          items.add(new ListItem(ListItem.TYPE_SHADOW_TOP));
         }
+        items.add(new ListItem(ListItem.TYPE_RADIO_SETTING, R.id.btn_experiment, 0, R.string.Experiment_PeerIds).setLongValue(Settings.EXPERIMENT_FLAG_SHOW_PEER_IDS));
+        items.add(new ListItem(ListItem.TYPE_SHADOW_BOTTOM));
+        items.add(new ListItem(ListItem.TYPE_DESCRIPTION, 0, 0, R.string.Experiment_PeerIdsInfo));
 
         if (Config.EDGE_TO_EDGE_AVAILABLE) {
           if (!items.isEmpty()) {
